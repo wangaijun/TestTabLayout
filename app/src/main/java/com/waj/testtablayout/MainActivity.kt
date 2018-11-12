3fb419e3e2cbbb6a629adf8713ffe85f329cd798
waj <wangaj@glodon.com>
2018-11-12 14:43:02 +0800

    显示tablayout

--- app/src/main/java/com/waj/testtablayout/MainActivity.kt
+++ app/src/main/java/com/waj/testtablayout/MainActivity.kt
@@ -2,11 +2,16 @@ package com.waj.testtablayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"))
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"))
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"))
     }
 }
---
+++ readme.txt
@@ -0,0 +1,3 @@
+参考链接：https://www.jianshu.com/p/2b2bb6be83a8
+什么是TabLayout?
+TabLayout提供了一个水平的布局来展示Tabs
